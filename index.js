e5695b46ee96cdcdec17879fad5e5fc924d232a5
eustachekamala <eustachejared25@gmail.com>
2024-07-07 10:16:26 +0300

    I done the assignment of the first class function, that I create many methods to manipulate the array of the drivers

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers (){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = () => {
+    return drivers.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (fareMultiplier) => {
+    return (value) => {
+        return fareMultiplier * value;    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const fareQuintupler = createFareMultiplier(4);
+
+function selectDifferentDrivers(drivers, func){
+   return func(drivers);
+} 
+
+returnFirstTwoDrivers();
+returnLastTwoDrivers();
+createFareMultiplier();
+selectDifferentDrivers();
